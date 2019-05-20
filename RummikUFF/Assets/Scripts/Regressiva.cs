8550669c2b7988bd7eda00d64063f0be1a68b59a
Carlos Eduardo Menezes Junior <carloseduardojunior@id.uff.br>
2019-05-20 17:38:31 -0300

    Versão 2

---
+++ RummikUFF/Assets/Scripts/Regressiva.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Regressiva : MonoBehaviour
+{
+    public Text display;
+    public float contador = 30;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (contador > 0)
+        {
+            contador -= Time.deltaTime;
+            display.text = contador.ToString("f0");
+        }
+    }
+}
